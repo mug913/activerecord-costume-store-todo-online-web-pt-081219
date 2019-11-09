0a1514ffb0837ceee7e99c35a3c1ab5ad32eb599
Brad Alexander <brad.alexander913@gmail.com>
2019-11-09 18:18:37 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,7 +2,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
 def change
   create_table :haunted_houses do |t|
     t.string :name
-    t.integer :costume_inventory
+    t.string :theme
     t.string :location
     t.integer :num_of_employees
     t.boolean :still_in_business
