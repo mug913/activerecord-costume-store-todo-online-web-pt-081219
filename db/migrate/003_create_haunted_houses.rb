6b610c2546283eafb7dee75b2620dafc20378830
Brad Alexander <brad.alexander913@gmail.com>
2019-11-09 18:08:21 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,8 +6,8 @@ def change
     t.string :location
     t.integer :num_of_employees
     t.boolean :still_in_business
-    t.time :opening_time
-    t.time :closing_time
+    t.datetime :opening_time
+    t.datetime :closing_time
     t.timestamps
   end
 end
